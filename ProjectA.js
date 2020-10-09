1765e699eae04e25fb7f4a3949e0b7f962cc2fb3
dmarquette2022 <davidmarquette2022@u.northwestern.edu>
2020-10-09 01:08:04 -0500

    Created some more shapes and rotations

--- ProjectA.js
+++ ProjectA.js
@@ -40,46 +40,52 @@ var g_modelMatLoc;                  // that uniform's location in the GPU
 var g_isRun = true;                 // run/stop for animation; used in tick().
 var g_lastMS = Date.now();    			// Timestamp for most-recently-drawn image; 
 
-var main_rpm = 5.0;          // rotation speed, in degrees/second
+var g_mainRpm = 30.0;          // rotation speed, in degrees/second
+var g_mainAngle = 0;
+
 									
-var tail_rpm = 5.0;
+var g_tailRpm = 5.0;
+var g_tailAngle = 0.0;
 
 
 function main() {
 
-																						// Get gl, the rendering context for WebGL, from our 'g_canvas' object
-																						gl = getWebGLContext(g_canvas);
-																						if (!gl) {
-																							console.log('Failed to get the rendering context for WebGL');
-																							return;
-																						}
+	// Get gl, the rendering context for WebGL, from our 'g_canvas' object
+	gl = getWebGLContext(g_canvas);
 
-																						// Initialize shaders
-																						if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-																							console.log('Failed to intialize shaders.');
-																							return;
-																						}
+	if (!gl) {
+		console.log('Failed to get the rendering context for WebGL');
+		return;
+	}
 
-																						// Initialize a Vertex Buffer in the graphics system to hold our vertices
-																						g_maxVerts = initVertexBuffer(gl);  
-																						if (g_maxVerts < 0) {
-																							console.log('Failed to set the vertex information');
-																							return;
-																						}
+	// Initialize shaders
+	if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+		console.log('Failed to intialize shaders.');
+		return;
+	}
 
-																						window.addEventListener("keydown", myKeyDown, false);
-																						window.addEventListener("keyup", myKeyUp, false);
+	// Initialize a Vertex Buffer in the graphics system to hold our vertices
+	g_maxVerts = initVertexBuffer(gl);  
+	if (g_maxVerts < 0) {
+		console.log('Failed to set the vertex information');
+		return;
+	}
 
-																						gl.clearColor(0.3, 0.3, 0.3, 1.0);
-																						gl.depthFunc(gl.LESS);
-																						gl.enable(gl.DEPTH_TEST); 	  
-																						
-																					// Get handle to graphics system's storage location of u_ModelMatrix
-																					g_modelMatLoc = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
-																					if (!g_modelMatLoc) { 
-																						console.log('Failed to get the storage location of u_ModelMatrix');
-																						return;
-																					}
+	window.addEventListener("keydown", myKeyDown, false);
+	window.addEventListener("keyup", myKeyUp, false);
+	gl.clearColor(0.3, 0.3, 0.3, 1.0);
+
+	gl.depthFunc(gl.LESS);
+	gl.enable(gl.DEPTH_TEST); 	  
+
+	// Get handle to graphics system's storage location of u_ModelMatrix
+	g_modelMatLoc = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+
+	if (!g_modelMatLoc) 
+	{ 
+		console.log('Failed to get the storage location of u_ModelMatrix');
+		return;
+	}
 
   var tick = function() {
 	animate();
@@ -88,115 +94,252 @@ function main() {
   };
 
   tick();
-	
 }
 
 function initVertexBuffer() {				 
 
   var colorShapes = new Float32Array([
-	 0, 0, 0, 1, 	0, 0, 0,
-	 1, 0, 0, 1, 	0, 0, 0,
-	 1, 0.2, 0, 1,	 0, 0, 0,
+	//Proppeler
+	0, 0, 0, 1, 	0, 0, 0,
+	1, -0.1, 0, 1, 	0, 0, 0,
+	1, 0.1, 0, 1,	 0, 0, 0,
 
-	 0, 0, 0, 1, 	0, 0, 0,
-	 0, 1, 0, 1, 	0, 0, 0,
-	 -0.2, 1, 0, 1,	 0, 0, 0,
+	1, -0.1, 0, 1, 	0, 0, 0,
+	1, 0.1, 0, 1,	 0, 0, 0,
+	1, 0.1, -0.5, 1,	 0, 0, 0,
 
-	 0, 0, 0, 1, 	0, 0, 0,
-	 0, -1, 0, 1, 	0, 0, 0,
-	 0.2, -1, 0, 1,	 0, 0, 0,
+	1, -0.1, 0, 1,	 0, 0, 0,
+	1, -0.1, -0.5, 1,	 0, 0, 0,
+	1, 0.1, -0.5, 1,	 0, 0, 0,
 
-	 0, 0, 0, 1, 	0, 0, 0,
-	 -1, 0, 0, 1, 	0, 0, 0,
-	 -1, -0.2, 0, 1,	 0, 0, 0
-	 
+	0, 0, 0, 1, 	0, 0, 1,
+	1, -0.1, -0.5, 1, 	0, 0, 1,
+	1, 0.1, -0.5, 1,	 0, 0, 1,
+
+	0, 0, 0, 1, 	1, 0, 0,
+	1, 0.1, 0, 1,	 1, 0, 0,
+	1, 0.1, -0.5, 1,	 1, 0, 0,
+
+	0, 0, 0, 1, 	0, 1, 0,
+	1, -0.1, 0, 1,	 0, 1, 0,
+	1, -0.1, -0.5, 1,	 0, 1, 0,
+
+
+
+	/*
+
+	0, 0, 0, 1, 	0, 0, 0,
+	0, 1, 0, 1, 	0, 0, 0,
+	-0.2, 1, 0, 1,	 0, 0, 0,
+
+	0, 0, 0, 1, 	0, 0, 0,
+	0, -1, 0, 1, 	0, 0, 0,
+	0.2, -1, 0, 1,	 0, 0, 0,
+
+	0, 0, 0, 1, 	0, 0, 0,
+	-1, 0, 0, 1, 	0, 0, 0,
+	-1, -0.2, 0, 1,	 0, 0, 0,
+	*/
+
+
+//Body
+
+//front1,0,0,
+	0.0, 0.0, 0.0, 1.0, 	1,0,0,
+	0.0,  1.0, 0.0, 1.0,  	1,0,0,
+	1.0,  1.0, 0.0, 1.0, 	1,0,0,
+
+	0.0, 0.0,  0.0, 1.0, 	1,0,0, 
+	1.0,  0.0,  0.0, 1.0,   1,0,0,
+	1.0,  1.0,  0.0, 1.0, 	1,0,0,
+
+	0.0, 0.0,  1.0, 1.0, 	0,1,0,
+	0.0,  1.0,  1.0, 1.0,  	0,1,0,
+	1.0,  1.0,  1.0, 1.0, 	0,1,0,
+
+	0.0, 0.0,  1.0, 1.0, 	0,1,0, 
+	1.0,  0.0,  1.0, 1.0,   0,1,0,
+	1.0,  1.0,  1.0, 1.0, 	0,1,0,
+
+	1.0, 0.0,  0.0, 1.0, 	0,0,1, 
+	1.0,  1.0,  0.0, 1.0,   0,0,1,
+	1.0,  1.0,  1.0, 1.0, 	0,0,1, 
+
+	1.0, 0.0,  0.0, 1.0, 	0,0,1, 
+	1.0,  0.0,  1.0, 1.0,   0,0,1,
+	1.0,  1.0,  1.0, 1.0, 	0,0,1,
+
+	0.0, 0.0,  0.0, 1.0, 	1,0,1, 
+	0.0,  1.0,  0.0, 1.0,   1,0,1,
+	0.0,  1.0,  1.0, 1.0, 	1,0,1,
+
+	0.0, 0.0,  0.0, 1.0, 	1,0,1, 
+	0.0,  0.0,  1.0, 1.0,   1,0,1,
+	0.0,  1.0,  1.0, 1.0, 	1,0,1, 
+
+	0.0, 1.0,  0.0, 1.0, 	1,1,0, 
+	0.0,  1.0,  1.0, 1.0,   1,1,0,
+	1.0,  1.0,  1.0, 1.0, 	1,1,0, 
+
+	0.0, 1.0,  0.0, 1.0, 	1,1,0, 
+	1.0,  1.0,  0.0, 1.0,   1,1,0,
+	1.0,  1.0,  1.0, 1.0, 	1,1,0,
+
+	0.0, 0.0,  0.0, 1.0, 	1,1,0.5, 
+	0.0,  0.0,  1.0, 1.0,   1,1,0.5,
+	1.0,  0.0,  1.0, 1.0, 	1,1,0.5, 
+
+	0.0, 0.0,  0.0, 1.0, 	1,1,0.5, 
+	1.0,  0.0,  0.0, 1.0,   1,1,0.5,
+	1.0,  0.0,  1.0, 1.0, 	1,1,0.5,
+
+	//back
+	0.0, 0.0, 0.0, 1.0, 	0.0, 0, 1, 
+	0.0,  1.0, 0.0, 1.0,  	0.0, 0, 1,
+	1.0,  1,  -0.2, 1.0,    0.0, 0, 1.0,
+
+	0.0, 0.0, -1.0, 1.0, 	0.0, 1.0, 0, 
+	0.0,  1.0, -1.0, 1.0,  	0.0, 1.0, 0,
+	1.0,  1,  -0.2, 1.0,    0.0,1.0,0,
+
+	0.0, 0.0, 0.0, 1.0, 	0.3, 1, 0.5, 
+	0.0, 0.0, -1.0, 1.0, 	0.3, 1, 0.5,
+	1.0,  1,  -0.2, 1.0,    0.3, 1, 0.5,
+
+	0.0,  1.0, 0.0, 1.0,  1, 0, 0,
+	0.0,  1.0, -1.0, 1.0,  1, 0, 0,
+	1.0,  1,  -0.2, 1.0,   1, 0, 0,
+
   ]);
 
-  g_vertsMax = 12;
+  g_vertsMax = 60;
+
+  var shapeBufferHandle = gl.createBuffer();  
+  if (!shapeBufferHandle) {
+    console.log('Failed to create the shape buffer object');
+    return false;
+  }
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, shapeBufferHandle);
+  gl.bufferData(gl.ARRAY_BUFFER, colorShapes, gl.STATIC_DRAW);
+
+  var FSIZE = colorShapes.BYTES_PER_ELEMENT; 
+
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return -1;
+  }
+
+  gl.vertexAttribPointer(a_Position, 4, gl.FLOAT, false, FSIZE * 7, 0);						
+  									
+  gl.enableVertexAttribArray(a_Position);  
+  									// Enable assignment of vertex buffer object's position data
+
+  // Get graphics system's handle for our Vertex Shader's color-input variable;
+  var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+  if(a_Color < 0) {
+    console.log('Failed to get the storage location of a_Color');
+    return -1;
+  }
   
-	
-															// Create a buffer object
-															var shapeBufferHandle = gl.createBuffer();  
-															if (!shapeBufferHandle) {
-																console.log('Failed to create the shape buffer object');
-																return false;
-															}
+  // Use handle to specify how to retrieve color data from our VBO:
+  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 7, FSIZE * 4);
+  									
+  gl.enableVertexAttribArray(a_Color);  
 
-															gl.bindBuffer(gl.ARRAY_BUFFER, shapeBufferHandle);
-
-															gl.bufferData(gl.ARRAY_BUFFER, colorShapes, gl.STATIC_DRAW);
-
-															var FSIZE = colorShapes.BYTES_PER_ELEMENT;
-																
-															var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-															if (a_Position < 0) {
-																console.log('Failed to get the storage location of a_Position');
-																return -1;
-															}
-															// Use handle to specify how to retrieve position data from our VBO:
-															gl.vertexAttribPointer(a_Position,4,gl.FLOAT, false,FSIZE * 7,0);
-
-															gl.enableVertexAttribArray(a_Position);  
-
-															// Get graphics system's handle for our Vertex Shader's color-input variable;
-															var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
-															if(a_Color < 0) {
-																console.log('Failed to get the storage location of a_Color');
-																return -1;
-															}
-															// Use handle to specify how to retrieve color data from our VBO:
-															gl.vertexAttribPointer(a_Color,3,gl.FLOAT,false,FSIZE * 7,FSIZE * 4);
-																								
-															gl.enableVertexAttribArray(a_Color);  
-
-															gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
 }
 
 
-function drawAll() {
-
+function drawAll() 
+{
   	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 	clrColr = new Float32Array(4);
 	clrColr = gl.getParameter(gl.COLOR_CLEAR_VALUE);
+	
 
-	g_modelMatrix.rotate(-main_rpm, 0, 0, 1);
+	g_modelMatrix.setTranslate(-0.5,-0.5,0,0);
+	g_modelMatrix.scale(1,1,-1);
+	g_modelMatrix.scale(0.5,0.5,0.5);
+	g_modelMatrix.rotate(g_tailAngle,0,1,0);
+	//REMOVE THIS TO SEE REGULAR POSITIONING
+	g_modelMatrix.rotate(45,1,0,0);
 	gl.uniformMatrix4fv(g_modelMatLoc, false, g_modelMatrix.elements);
-	  
+	drawBody();
+	pushMatrix(g_modelMatrix);
+	g_modelMatrix.translate(0.5,1.1,0.5,0);
+	g_modelMatrix.rotate(g_mainAngle,0,1,0);
+	//g_modelMatrix.rotate(90, 1, 0, 0);
+
+	gl.uniformMatrix4fv(g_modelMatLoc, false, g_modelMatrix.elements);
 	drawPropeller();
+	g_modelMatrix.rotate(90, 0,1,0);
+	gl.uniformMatrix4fv(g_modelMatLoc, false, g_modelMatrix.elements);
+	drawPropeller();
+	g_modelMatrix.rotate(90, 0,1,0);
+	gl.uniformMatrix4fv(g_modelMatLoc, false, g_modelMatrix.elements);
+	drawPropeller();
+	g_modelMatrix.rotate(90, 0,1,0);
+	gl.uniformMatrix4fv(g_modelMatLoc, false, g_modelMatrix.elements);
+	drawPropeller();
+	g_modelMatrix = popMatrix();
+	g_modelMatrix.translate(1,0,1,0);
+	gl.uniformMatrix4fv(g_modelMatLoc, false, g_modelMatrix.elements);
+	drawBack();
+
 }
 
-function drawPropeller(){
-	gl.drawArrays(gl.TRIANGLES, 0,12);
+function drawBody()
+{
+	gl.drawArrays(gl.TRIANGLES, 18,36);
+}
+
+function drawPropeller()
+{
+	gl.drawArrays(gl.TRIANGLES, 0,18);
+}
+
+function drawBack()
+{
+	gl.drawArrays(gl.TRIANGLES, 54,12);
 }
 
 var g_last = Date.now();
 
-function animate() {
-
+function animate() 
+{
 	var now = Date.now();
 	var elapsed = now - g_last;
 	g_last = now;
-
+	g_mainAngle = g_mainAngle + (g_mainRpm * elapsed) / 1000;
+	g_tailAngle = g_tailAngle + (g_tailRpm * elapsed) / 1000; 
 }
 
 function slowMain(){
-	if (main_rpm == 0) return
-	main_rpm --
+	if (g_mainRpm == 0)
+	{
+		return;
+	}
+	g_mainRpm -= 10;
 }
 
 function speedMain(){
-	main_rpm ++
+	g_mainRpm += 10;
 }
 
 function slowTail(){
-	if (tail_rpm == 0) return
-	tail_rpm --
+	if (g_tailRpm == 0)
+	{
+		return;
+	}
+	g_tailRpm -= 5;
 }
 
 function speedTail(){
-	tail_rpm ++
+	g_tailRpm += 5;
 }
 //==================HTML Button Callbacks======================
 
